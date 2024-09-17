d033e09ceecdbc6db41c82d5d9a364b10e6a54e0
RedSunRnD <vvatutin.rs@gmail.com>
2024-09-18 00:26:20 +0300

    Corrected homework

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача #1");
-        String firstName = "Иванов";
-        String middleName = "Семён";
-        String lastName = "Семёнович";
-        String fullName = firstName + ' ' + middleName + ' ' + lastName;
-        System.out.println(fullName);
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
         System.out.println();
         System.out.println("Задача #2");
         String fullNameUp = fullName.toUpperCase();
